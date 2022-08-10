c43c266229681a8164bf0f0c0903763a9f2381f8
shockValue666 <mapeDeveloper@protonmail.com>
2022-08-11 00:40:49 +0300

    idek

--- src/comps/CardDefinition.tsx
+++ src/comps/CardDefinition.tsx
@@ -16,16 +16,16 @@ function CardDefinition() {
       <Box sx={{display:"flex",justifyContent:"space-around", padding:"100px",flexWrap:"wrap",rowGap:"60px",marginTop:"10%"}}>
             <Card style={{ minWidth: 275, maxWidth:500,background:"rgba(204, 199, 184,0.5)"}}>
                         <CardContent sx={{background:"rgba(204, 199, 184,0.5)"}}>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                            <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
                             Word of the Day
                             </Typography>
-                            <Typography variant="h5" component="div">
+                            <Typography variant="h4" component="div" style={{fontSize:"25px"}}>
                             mis{bull}​an{bull}​thrope
                             </Typography>
-                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                            <Typography sx={{ mb: 1.5,fontSize:20 }} color="text.secondary">
                             noun
                             </Typography>
-                            <Typography variant="body2">
+                            <Typography variant="h3" style={{fontSize:"25px"}}>
                             a person who hates or distrusts humankind
                             <br />
                             {'"The misanthrope hates people, believes they deserve to suffer, and wants to harm them when possible."'}
@@ -35,7 +35,7 @@ function CardDefinition() {
                             <Button size="small">Learn More</Button>
                         </CardActions> */}
                     </Card>
-                    <img src="https://somespace.fra1.digitaloceanspaces.com/IMG_3429.png" height={"281px"} width="200px" alt="cent" style={{backgroundColor:"black"}}/>
+                    <img src="https://somespace.fra1.digitaloceanspaces.com/misanthrope.jpg" height={"281px"} width="230px" alt="cent" style={{backgroundColor:"black"}}/>
             </Box>
   )
 }
--- src/comps/MintButtonWrapper.tsx
+++ src/comps/MintButtonWrapper.tsx
@@ -6,21 +6,31 @@ import TypeWritter from './TypeWriter'
 function MintButtonWrapper({children}:any) {
   return (
     <div style={{border:"2px solid black",display:"flex",justifyContent:"center",alignItems:"center",backgroundImage:"url(https://somespace.fra1.digitaloceanspaces.com/Laboratorium%20%281%292.jpeg)",backgroundRepeat:"no-repeat",backgroundSize:"cover"}}>
-            <Grid container spacing={2} height="750px" padding={5}  >
-                <Grid item xs={1} >
-                </Grid>
-                <Grid item xs={10} >
+            <Grid container spacing={2} height="750px" padding={5} justifyContent="center" alignItems="center">
+                {/* <Grid item xs={1} >
+                </Grid> */}
+                {/* <Grid item xs={10} > */}
                     {/* <Box sx={{border:"10px solid white",backgroundImage:"url(https://via.placeholder.com/728x900.png)"}} maxWidth="md" width="100%" height="100%" textAlign={"center"}> */}
-                    <Box  maxWidth="lg" width="100%" height="100%" textAlign={"center"}>
+                    {/* <Box  maxWidth="lg" width="100%" height="100%" textAlign={"center"}> */}
+                        {/* <a href="https://placeholder.com"> <img src="https://via.placeholder.com/550" alt="p" /> </a> */}
+                          {/* <TypeWritter/>
+                        {children}
+                        <p style={{color:"white"}}></p> */}
+                        {/* <MintButton/> */}
+                    {/* </Box>
+                </Grid> */}
+                {/* <Grid item xs={1}>
+
+                </Grid> */}
+                <Grid item  width="lg">
+                    {/* <Box sx={{border:"10px solid white",backgroundImage:"url(https://via.placeholder.com/728x900.png)"}} maxWidth="md" width="100%" height="100%" textAlign={"center"}> */}
+                    <Box  maxWidth="lg" style={{minWidth:"500px"}} width="100%" height="100%" textAlign={"center"}>
                         {/* <a href="https://placeholder.com"> <img src="https://via.placeholder.com/550" alt="p" /> </a> */}
                           <TypeWritter/>
                         {children}
                         <p style={{color:"white"}}></p>
                         {/* <MintButton/> */}
                     </Box>
-                </Grid>
-                <Grid item xs={1}>
-
                 </Grid>
             </Grid>
     </div>
