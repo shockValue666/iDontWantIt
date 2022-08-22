aed956f94820e1b6d42ff0418e6fb2e7ae7e9f33
shockValue666 <mapeDeveloper@protonmail.com>
2022-08-22 14:30:24 +0300

    s'

--- src/comps/DesktopNavbar.tsx
+++ src/comps/DesktopNavbar.tsx
@@ -22,7 +22,7 @@ function DesktopNavbar() {
           </Link>
 
             {/* <Button variant="contained" size="large" style={{background:"rgba(66, 245, 236,0.1)",color:"white"}}> */}
-            <Button target="_blank" href="https://staking.depravedscientists.xyz/" variant="contained" size="large" style={{background:"rgba(201,55,173,255)",color:"white",fontWeight:"900",paddingLeft:"5%",paddingRight:"5%"}}>
+            <Button target="_blank" href="https://staking.depravedscientists.online/" variant="contained" size="large" style={{background:"rgba(201,55,173,255)",color:"white",fontWeight:"900",paddingLeft:"5%",paddingRight:"5%"}}>
               staking
             </Button>
 
